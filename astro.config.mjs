980c942bb5a8311f7edf9f28421d4c4cb97257d9
dairomorales5 <damoralesj@unal.edu.co>
2024-04-27 19:39:57 -0500

    Agregar sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'http://leonia.com.co',
+  integrations: [react(), sitemap()]
 });
